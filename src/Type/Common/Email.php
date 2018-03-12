7ad5917d64ef6a451f65dc400f9ce0b6162733f6
Roman Khimin <roman.khimin@lemberg.co.uk>
2018-03-12 18:15:00 +0200

    Updated Email class.

--- src/Type/Common/Email.php
+++ src/Type/Common/Email.php
@@ -15,14 +15,22 @@ class Email {
   protected $DefaultInd;
 
   /**
+   * @var string|null
+   */
+  protected $ShareMarketInd;
+
+  /**
    * Email constructor.
    *
    * @param string|null $DefaultInd
+   * @param string|null $ShareMarketInd
    */
   public function __construct(
-    ?string $DefaultInd
+    ?string $DefaultInd,
+    ?string $ShareMarketInd
   ) {
     $this->DefaultInd = $DefaultInd;
+    $this->ShareMarketInd = $ShareMarketInd;
   }
 
 }
